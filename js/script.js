d3bdc14e169463e8811292a4e50c5013f502dda2
chrohan0424x <chaudharirohan2825@gmail.com>
2023-08-20 21:39:57 +0530

    Port Official

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Design","Youtuber","App Creater","UI/UX Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
